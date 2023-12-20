eaf011ab0c6d50e0f3139b976f1de793f431da5e
VladislavKovpak <vladislavkovpak1@gmail.com>
2023-12-20 08:41:04 +0100

    fix conflicts

--- Startup.cs
+++ Startup.cs
@@ -37,11 +37,6 @@ namespace TaskAuthenticationAuthorization
             services.AddDbContext<ShoppingContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options => 
-            {
-                options.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login");
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
